e8f1899314369867444b374c111382e390a42bb2
William Grand <ggrand@alum.mit.edu>
2023-03-27 14:04:12 -0400

    [Quat/Vec] Add operators to Quaternion and Vector

--- model/Quaternion.h
+++ model/Quaternion.h
@@ -1,4 +1,56 @@
+#include <cmath>
 
 struct Quaternion { 
    float w, x, y, z;
+
+   // Normalize the quaternion
+   void Normalize() {
+      const float length = Length();
+      w /= length;
+      x /= length;
+      y /= length;
+      z /= length;
+   }
+
+   float Length() { 
+      return std::sqrt(w*w + x*x + y*y + z*z);
+   }
+
+   Quaternion operator+(const Quaternion &q) const {
+      return {w + q.w, x + q.x, y + q.y, z + q.z};
+   }
+   Quaternion operator-(const Quaternion& q) const {
+      return {w - q.w, x - q.x, y - q.y, z - q.z};
+   }
+   
+   Quaternion operator*(const Quaternion& q) const {
+      return { w*q.w - x*q.x - y*q.y - z*q.z,
+               w*q.x + x*q.w - y*q.z + z*q.y,
+               w*q.y + x*q.z + y*q.w - z*q.x,
+               w*q.z - x*q.y + y*q.x + z*q.w};
+   //    return { w*q.w - x*q.x - y*q.y - z*q.z,
+   //             w*q.x + x*q.w + y*q.z - z*q.y,
+   //             w*q.y - x*q.z + y*q.w + z*q.x,
+   //             w*q.z + x*q.y - y*q.x + z*q.w};
+   }
+
+   Quaternion operator*(float s) const {
+      return {w*s, x*s, y*s, z*s};
+   }
+   Quaternion operator/(float s) const {
+      return {w/s, x/s, y/s, z/s};
+   }
+   void operator+=(const Quaternion& q) {
+      w += q.w; x += q.x; y += q.y; z += q.z;
+   }
+   void operator-=(const Quaternion& q) {
+      w -= q.w; x -= q.x; y -= q.y; z -= q.z;
+   }
+   bool operator==(const Quaternion& q) const {
+      return (w == q.w) && (x == q.x) && (y == q.y) && (z == q.z);
+   }
+   bool operator!=(const Quaternion& q) const {
+      return (w != q.w) || (x != q.x) || (y != q.y) || (z != q.z);
+   }
+
 };
--- model/Vector.h
+++ model/Vector.h
@@ -1,4 +1,57 @@
 
 struct Vector { 
    float x, y, z;
+
+   // Normalize the vector
+   void Normalize() {
+      const float length = Length();
+      x /= length;
+      y /= length;
+      z /= length;
+  }
+
+   float Length() { 
+      return std::sqrt(x*x + y*y + z*z);
+   }
+
+   Vector operator+(const Vector& v) const {
+      return {x + v.x, y + v.y, z + v.z};
+   }
+   Vector operator-(const Vector& v) const {
+      return {x - v.x, y - v.y, z - v.z};
+   }
+   
+   // Multiplication by scalar operator (vector * scalar)
+   Vector operator*(float s) const {
+      return {x*s, y*s, z*s};
+   }
+
+   // Division by scalar operator (vector / scalar)
+   Vector operator/(float s) const {
+      return {x/s, y/s, z/s};
+   }
+
+   // Dot product operator (vector1 dot vector2)
+   float operator*(const Vector& v) const {
+      return x*v.x + y*v.y + z*v.z;
+   }
+   
+   // Cross product operator (vector1 cross vector2)
+   Vector operator^(const Vector& v) const {
+      return {y*v.z - z*v.y, z*v.x - x*v.z, x*v.y - y*v.x};
+   }
+   
+   void operator+=(const Vector& v) {
+      x += v.x; y += v.y; z += v.z;
+   }
+   void operator-=(const Vector& v) {
+      x -= v.x; y -= v.y; z -= v.z;
+   }
+   bool operator==(const Vector& v) const {
+      return (x == v.x) && (y == v.y) && (z == v.z);
+   }
+   bool operator!=(const Vector& v) const {
+      return (x != v.x) || (y != v.y) || (z != v.z);
+   }
+
 };
