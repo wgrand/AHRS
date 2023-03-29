dfaf27103ab2ba78698dc0e6f530989a29f50786
William Grand <ggrand@alum.mit.edu>
2023-03-29 11:03:54 -0400

    [AHRS] Change from static property and static function

--- complementary_filter/AHRS.cpp
+++ complementary_filter/AHRS.cpp
@@ -1,18 +1,30 @@
-#include <iostream>
-#include <string>
-
 #include "AHRS.h"
 
 using namespace std;
 
 class AHRS {
-   
+
+private:
+
+   Quaternion AttitudePropagation(Quaternion q_prior, Vector omega, float dt) {
+      Quaternion q_omega = {1, -0.5*dt*omega.x, -0.5*dt*omega.y, -0.5*dt*omega.z };
+      Quaternion q = q_omega*q_prior;
+      q.Normalize();
+      return q;
+   }
+   Quaternion AM_Estimation(Vector acc, Vector mag, string frame) {
+      Mat3x3 R = RotationMatrix(acc, mag, frame);
+      return Quaternion(R);
+   }
+
 public:
 
-   static const float gain = 0.995;
-   static const float gyro_bias = 0.02;
+   float gain = 0.995;
+   float gyro_bias = 0.02;
 
-   static Quaternion Update(Quaternion q_prior, Vector gyr, Vector acc, Vector mag, float dt, string frame) {
+   AHRS() { }
+
+   Quaternion Update(Quaternion q_prior, Vector gyr, Vector acc, Vector mag, float dt, string frame) {
 
       Quaternion q;
 
@@ -25,31 +37,16 @@ public:
       // fuse predicted and updated quaternions
       Quaternion sum_q = q_omega + q_am;
       
-      float len_q = sum_q.Length();
-      if (len_q < sqrt(2)) {
-         q = q_omega*AHRS::gain - q_am*(1 - AHRS::gain); // if the components oppose each other, then they may add up to less than sqrt(2)
+      if (sum_q.Length() < sqrt(2)) {
+         q = q_omega*gain - q_am*(1 - gain); // if the components oppose each other, then they may add up to less than sqrt(2)
       } else { 
-         q = q_omega*AHRS::gain + q_am*(1 - AHRS::gain);
+         q = q_omega*gain + q_am*(1 - gain);
       }
 
       q.Normalize();
 
       return q;
 
-   }
-
-private:
-
-   static Quaternion AttitudePropagation(Quaternion q_prior, Vector omega, float dt) {
-      Quaternion q_omega = {1, -0.5*dt*omega.x, -0.5*dt*omega.y, -0.5*dt*omega.z };
-      Quaternion q = q_omega*q_prior;
-      q.Normalize();
-      return q;
-   }
-   static Quaternion AM_Estimation(Vector acc, Vector mag, string frame) {
-      Mat3x3 R = RotationMatrix(acc, mag, frame);
-      Quaternion q = Quaternion(R);
-      return q;
    }
 
 };
--- complementary_filter/AHRS.h
+++ complementary_filter/AHRS.h
@@ -4,18 +4,18 @@
 #include "../model/Quaternion.h"
 #include "../model/Vector.h"
 #include "../model/Mat3x3.h"
-
 #include "../model/RotationMatrix.h"
 
 using namespace std;
 
 class AHRS {
-public:
-   static const float gain;
-   static const float gyro_bias;
-   static Quaternion Update(Quaternion q_prior, Vector gyr, Vector acc, Vector mag, float dt, string frame);
-
 private:
-   static Quaternion AttitudePropagation(Quaternion q_prior, Vector omega, float dt);
-   static Quaternion AM_Estimation(Vector acc, Vector mag, string frame);
+   Quaternion AttitudePropagation(Quaternion q_prior, Vector omega, float dt);
+   Quaternion AM_Estimation(Vector acc, Vector mag, string frame);
+
+public:
+   float gain = 0.995;
+   float gyro_bias = 0.02;
+   AHRS() { }
+   Quaternion Update(Quaternion q_prior, Vector gyr, Vector acc, Vector mag, float dt, string frame) {};
 };
