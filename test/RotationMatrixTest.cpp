19f4092e4540beafce9b2af396c6bd737e3a7fea
William Grand <ggrand@alum.mit.edu>
2023-04-01 00:31:30 -0400

    [UnitTest] Add tests for RotationMatrix

---
+++ test/RotationMatrixTest.cpp
@@ -0,0 +1,44 @@
+#include <string>
+#include <iostream>
+#include <cmath>
+
+#include "gtest/gtest.h"
+#include "../model/Vector.h"
+#include "../model/Quaternion.h"
+#include "../model/Mat3x3.h"
+#include "../model/RotationMatrix.h"
+
+TEST(RotationMatrixTest, Identity) {
+
+    // Test case 1: NED frame
+
+    // Define an acceleration vector with gravity pointing down in the +z direction.
+    Vector acc(0.0, 0.0, 1.0);
+
+    // Define a magnetic field vector with the magnetic field pointing north in the +x direction.
+    Vector mag(1.0, 0.0, 0.0);
+
+    // Create a rotation matrix from the acceleration and magnetic field vectors.
+    Mat3x3 result = RotationMatrix(acc, mag, "NED");
+
+    // Define the expected result.
+    Mat3x3 expected_result = {1.0, 0.0, 0.0,
+                              0.0, 1.0, 0.0,
+                              0.0, 0.0, 1.0};
+
+    EXPECT_NEAR(result[0][0], expected_result[0][0], 0.0001);
+    EXPECT_NEAR(result[0][1], expected_result[0][1], 0.0001);
+    EXPECT_NEAR(result[0][2], expected_result[0][2], 0.0001);
+    EXPECT_NEAR(result[1][0], expected_result[1][0], 0.0001);
+    EXPECT_NEAR(result[1][1], expected_result[1][1], 0.0001);
+    EXPECT_NEAR(result[1][2], expected_result[1][2], 0.0001);
+    EXPECT_NEAR(result[2][0], expected_result[2][0], 0.0001);
+    EXPECT_NEAR(result[2][1], expected_result[2][1], 0.0001);
+    EXPECT_NEAR(result[2][2], expected_result[2][2], 0.0001);
+    
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
